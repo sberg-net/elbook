c81796a2f23aff84e44435362d6e2904e32eb2fa
basketmc <basketmc@gmail.com>
2025-03-06 08:49:31 +0100

    ## [2.8.0]
    ### Changed
    - Glossar erweitert um Spezialisierungen/Fachrichtungen
    - BugFix - Swagger

---
+++ src/main/java/net/sberg/elbook/glossarcmpts/SpecializationInfo.java
@@ -0,0 +1,12 @@
+package net.sberg.elbook.glossarcmpts;
+
+import lombok.Data;
+
+@Data
+public class SpecializationInfo {
+    private String code;
+    private String display;
+    private String definition;
+    private String description;
+    private String url;
+}
